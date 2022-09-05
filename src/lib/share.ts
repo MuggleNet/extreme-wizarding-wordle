a4ac7d5a9df088d9684f4d442edee448100b521d
MuggleNet <98475113+MuggleNet@users.noreply.github.com>
2022-09-05 17:29:20 -0400

    Updated to Extreme

--- src/lib/share.ts
+++ src/lib/share.ts
@@ -3,7 +3,7 @@ import { solutionIndex } from './words'
 
 export const shareStatus = (guesses: string[], lost: boolean) => {
   navigator.clipboard.writeText(
-    `#WizardingWordle ${solutionIndex} ${lost ? 'X' : guesses.length}/6\n\n` +
+    `Extreme #WizardingWordle ${solutionIndex} ${lost ? 'X' : guesses.length}/6\n\n` +
       generateEmojiGrid(guesses) +
       `\n\nPlay @MuggleNet's original Harry Potter Wordle: bit.ly/WizardingWordle`
   )
